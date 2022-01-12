f168a4e58579e3ec19804a1f219cf7eb483dd481
ginchan <2669686475@qq.com>
2022-01-12 11:36:55 +0800

    dwd层-提取公共代码

---
+++ dwd/src/main/java/com/shujia/dwd/SparkTool.scala
@@ -0,0 +1,38 @@
+package com.shujia.dwd
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.SparkSession
+
+/**
+ * 封装的spark 工具将通用的代码放到这个工具中
+ *
+ * @author chengjin
+ * @version 2022/1/12 11:15
+ */
+
+abstract class SparkTool extends Logging {
+
+  var day_id: String = _
+
+  def main(args: Array[String]): Unit = {
+    if (args.length == 0) {
+      log.error("请传入时间参数")
+    }
+
+    day_id = args(0)
+
+    log.info(s"时间参数为: $day_id")
+
+    log.info("创建spark环境")
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("DWDResReignMergeLocationMskDay")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    log.info("调用run方法")
+    this.run(spark)
+  }
+
+  def run(spark: SparkSession)
+}
